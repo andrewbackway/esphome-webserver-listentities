45879ba6efb359c32798cb058d5be984be888c18
andrewbackway <31836236+andrewbackway@users.noreply.github.com>
2025-09-12 16:29:51 +1000

    a

--- components/webserver-listentities/__init__.py
+++ components/webserver-listentities/__init__.py
@@ -1,3 +1,10 @@
 from esphome import config_validation as cv
+from esphome.const import CONF_ID
 
-CONFIG_SCHEMA = cv.Schema({})
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id("custom_web_server::WebServerListEntities"),
+})
+
+# Ensure ESPAsyncWebServer is included in the build
+DEPENDENCIES = ["web_server"]
+LIB_DEPS = ["esphome/ESPAsyncWebServer@2.7.0"]  # Matches ESPHome's bundled version
