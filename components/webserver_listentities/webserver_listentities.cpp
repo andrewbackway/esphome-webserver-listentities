60167c26f71087dd78cc1589e7f0b6ab40348f7d
andrewbackway <31836236+andrewbackway@users.noreply.github.com>
2025-09-12 20:11:17 +1000

    a

--- components/webserver_listentities/webserver_listentities.cpp
+++ components/webserver_listentities/webserver_listentities.cpp
@@ -12,7 +12,7 @@ static const char *const TAG = "webserver_listentities";
 // ESP-IDF web server handler (snake_case virtuals)
 class ListEntitiesHandler : public esphome::web_server_idf::AsyncWebHandler {
  public:
-  void canHandle(esphome::web_server_idf::AsyncWebServerRequest *request) override {
+  bool canHandle(esphome::web_server_idf::AsyncWebServerRequest *request) const override {
     return true;
     const auto url = request->url();
     const bool match = (url == "/entities" || url == "/entities/");
